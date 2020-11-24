6d532911408add066b6933a67231b4540287ec57
Mohammed Esafi <mesafi@student.1337.ma>
2020-11-24 07:24:56 +0100

    WARY

---
+++ popup/index.js
@@ -0,0 +1,12 @@
+// chrome.runtime.sendMessage({ command: "checkAuth" }, (response) => {
+//    console.log(response);
+// });
+
+document.getElementById("btn").addEventListener("click", (event) => {
+   // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+   //    let currentTab = tabs[0];
+   //    if (currentTab.url.includes("www.instagram.com")) {
+   //       chrome.tabs.executeScript(currentTab.id, { file: "content.js" });
+   //    }
+   // });
+});
