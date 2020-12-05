cca3281c20571f59cc79862f342ba61471ff56ad
Mohammed Esafi <mesafi@student.1337.ma>
2020-12-05 11:50:53 +0100

    content script __WARY__

--- content/index.js
+++ content/index.js
@@ -1,7 +1,44 @@
-const Likers = document.querySelector("body > div.RnEpo.Yx5HN > div > div > div.Igw0E.IwRSH.eGOV_.vwCYk.i0EQd > div > div");
+// Utils
+
+const sleep = (milliseconds) => {
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+// Likers
+
+var Likers = document.querySelector("body > div.RnEpo.Yx5HN > div > div > div.Igw0E.IwRSH.eGOV_.vwCYk.i0EQd > div");
 
 if (Likers) {
-   Likers.children.forEach((element) => {
-      console.log(element);
-   });
+	(async () => {
+		var index = 0;
+	
+		while (true)
+		{
+			var container = document.querySelector("body > div.RnEpo.Yx5HN > div > div > div.Igw0E.IwRSH.eGOV_.vwCYk.i0EQd > div > div");
+		
+			var children = container.children;
+
+			alert("length: " + children.length)
+		
+			if (children.length === index)
+				break ;
+		
+			while (index < children.length)
+			{
+				alert(index);
+
+				let button = children[index].querySelector('div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl > button');
+			
+				Likers.scroll({top: children[index].offsetTop, behavior: 'smooth'});
+
+				if (button.innerText === "Follow")
+				{
+					button.click();
+					await sleep(4000);
+				}
+					
+				++index;
+			}
+		}
+	})();
 }
